30ab0382df7bb2e683a1e5db618d67c2491dec1e
Danyl Lukashevych <danyllukashevych@gmail.com>
2022-08-26 19:55:24 +0300

    Refactor v3.1

--- index.php
+++ index.php
@@ -4,15 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <?php
-        include_once "includes/link.inc.php";  //cылки на css
+        include_once "includes/link.inc.php";
         include_once 'includes/heeds.inc.php';
         include_once 'includes/library.inc.php';
+        include_once 'includes/fonts.inc.php';
     ?>
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,200,0,0" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
     <div class="header">
@@ -37,12 +33,12 @@
             </ul>
         </div>
     </div>
-        <div class="content">
+    <div class="content">
             <?php
                 include_once 'interface/show_content.inc.php';
                 show_content($library, $get_library_section,$get_library_chapter,$get_chapter_name);
             ?>
-        </div>
+    </div>
     <div class="footer">
         &copy; MazFaKingMonster
     </div>
